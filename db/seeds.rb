52e93093f792adbea5eac43c73ca530298145eed
Amirsalar Heydari <amirsalaar@gmail.com>
2019-04-23 00:18:23 -0700

    Update the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+PASSWORD = 'supersecret'
 Comment.delete_all
 Post.delete_all
+User.delete_all
+
+User.create(
+    name: 'Amirsalar',
+    email: 'amirsalaar@gmail.com',
+    password: PASSWORD,
+    is_admin: true
+)
+
+10.times do
+    created_at = Faker::Date.backward(365) 
+    User.create(
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        created_at: created_at,
+        updated_at: created_at,
+        password: PASSWORD
+    )
+end
+
+users = User.all
 
 100.times do 
     created_at = Faker::Date.backward(365) 
@@ -15,13 +37,16 @@ Post.delete_all
         title: Faker::Food.dish,
         body: Faker::Hacker.say_something_smart,
         created_at: created_at,
-        updated_at: created_at
+        updated_at: created_at,
+        user: users.shuffle.sample
     )
     if p.valid?
         p.comments = rand(1..7).times.map do
             Comment.new(
                 name: Faker::Name.first_name,
-                body: Faker::Quote.famous_last_words
+                body: Faker::Quote.famous_last_words,
+                user: users.shuffle.sample,
+                created_at: created_at
             )
         end
     end
